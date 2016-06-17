f2d134b97f243eaba826c6b75674e37742972b3c
ghaddaa <ghadaibraheem92@gmail.com>
2016-06-17 23:33:53 +0300

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formatteName="ghada";
+var formattedRole="Software engineer";
+HTMLheaderName.replace("%dat%",formattedName);
+HTMLheaderRole.replace("%data%",formattedRole);
+$(#header).prepend(formattedName);
+$(#header).append(formattedRole);
